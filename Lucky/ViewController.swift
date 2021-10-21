baca25e83772005ecd96de7b4d1b9176020fddd1
Matthew Poelsterl <mpoelsterl1217@gmail.com>
2021-10-21 18:00:08 -0500

    Changed background color to yellow

--- Lucky/ViewController.swift
+++ Lucky/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Globals
+    var rand1 = Int.random(in: 1...6)
+    var rand2 = Int.random(in: 1...6)
+    
+    //Outlets
+    @IBOutlet weak var dieImage1: UIImageView!
+    @IBOutlet weak var dieImage2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        dieImage1.image = UIImage(named: "Dice\(rand1)")
+        dieImage2.image = UIImage(named: "Dice\(rand2)")
     }
 
+    //Actions
+    @IBAction func rollDice(_ sender: UIButton) {
+        rand1 = Int.random(in: 1...6)
+        rand2 = Int.random(in: 1...6)
+        dieImage1.image = UIImage(named: "Dice\(rand1)")
+        dieImage2.image = UIImage(named: "Dice\(rand2)")
+    }
+    
 
 }
 
